6c80156f959e9b78815cff235bfa0e0273d56d76
Gleb <fedorov22134@gmail.com>
2025-05-13 01:09:50 +0300

    add cout execption

--- main.cpp
+++ main.cpp
@@ -9,31 +9,38 @@ using namespace std;
 const int NotUsed = system("color F0");
 
 int main() {
-    Rhomb *rhomb = new Rhomb(120, 180, 80, 80);
-    rhomb->show();
-    Sleep(1000);
-    rhomb->hide();
-    Sleep(1000);
-    rhomb->move(250, 250);
-    Sleep(1000);
-    rhomb->hide();
-    Sleep(1000);
+    system("chcp 65001");
+    setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    MyRectangle *myRect = new MyRectangle(120, 180, 40);
-    myRect->show();
-    Sleep(1000);
-    myRect->hide();
-    Sleep(1000);
-    myRect->move(250, 250);
-    Sleep(1000);
-    myRect->hide();
-    Sleep(1000);
+    try {
+        Rhomb *rhomb = new Rhomb(120, 180, 70, 80);
+        rhomb->show();
+        Sleep(1000);
+        rhomb->hide();
+        Sleep(1000);
+        rhomb->move(250, 250);
+        Sleep(1000);
+        rhomb->hide();
+        Sleep(1000);
 
-    ComplexFigure *cf = new ComplexFigure(myRect, rhomb);
-    cf->show();
-    Sleep(2000);
-    cf->hide();
-    Sleep(2000);
-    cf->move(180, 360);
+        MyRectangle *myRect = new MyRectangle(120, 180, 40);
+        myRect->show();
+        Sleep(1000);
+        myRect->hide();
+        Sleep(1000);
+        myRect->move(250, 250);
+        Sleep(1000);
+        myRect->hide();
+        Sleep(1000);
+
+        ComplexFigure *cf = new ComplexFigure(myRect, rhomb);
+        cf->show();
+        Sleep(2000);
+        cf->hide();
+        Sleep(2000);
+        cf->move(180, 360);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
     Sleep(500000);
 }
